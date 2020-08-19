b84983878872baac24d72478cf851dd932e69edc
Greg <g.rutherford10@gmail.com>
2020-08-19 11:59:53 +0100

    created a basic maths question page

--- src/components/MathQuiz.js
+++ src/components/MathQuiz.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import './MathQuiz.css';
 
 function MathQuiz() {
   return (
-    <div>
-      <h2>Math</h2>
+    <div className="math-container">
+      <h2>Math Questions</h2>
+      <div className="questions-section">
+        <div className="question">
+          <p>10</p>
+          <p>+</p>
+          <input type="number" />
+          <p>=</p>
+          <p>20</p>
+          <button>Check</button>
+        </div>
+        <div className="question">
+          <p>5</p>
+          <p>x</p>
+          <input type="number" />
+          <p>=</p>
+          <p>20</p>
+          <button>Check</button>
+        </div>
+        <div className="question">
+          <p>18</p>
+          <p>-</p>
+          <input type="number" />
+          <p>=</p>
+          <p>9</p>
+          <button>Check</button>
+        </div>
+      </div>
     </div>
   );
 }
