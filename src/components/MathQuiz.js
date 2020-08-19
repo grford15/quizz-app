3ff454ec3f64d79d96dc91671f9573b907f55068
Greg <g.rutherford10@gmail.com>
2020-08-19 15:06:05 +0100

    add button to submit answers

--- src/components/MathQuiz.js
+++ src/components/MathQuiz.js
@@ -1,38 +1,51 @@
 import React from 'react';
 import './MathQuiz.css';
 
-function MathQuiz() {
-  return (
-    <div className="math-container">
-      <h2>Math Questions</h2>
-      <div className="questions-section">
-        <div className="question">
-          <p>10</p>
-          <p>+</p>
-          <input type="number" />
-          <p>=</p>
-          <p>20</p>
-          <button>Check</button>
+class MathQuiz extends React.Component {
+  state = {
+    answered: false,
+  };
+
+  answerQuestions = (e) => {
+    e.preventDefault();
+    console.log('Click !');
+  };
+
+  render() {
+    return (
+      <div className="math-container">
+        <h2>Math Questions</h2>
+        <div className="questions-section">
+          <div className="question">
+            <p>10</p>
+            <p>+</p>
+            <input type="number" />
+            <p>=</p>
+            <p>20</p>
+          </div>
+          <div className="question">
+            <p>5</p>
+            <p>x</p>
+            <input type="number" />
+            <p>=</p>
+            <p>20</p>
+          </div>
+          <div className="question">
+            <p>18</p>
+            <p>-</p>
+            <input type="number" />
+            <p>=</p>
+            <p>9</p>
+          </div>
         </div>
-        <div className="question">
-          <p>5</p>
-          <p>x</p>
-          <input type="number" />
-          <p>=</p>
-          <p>20</p>
-          <button>Check</button>
-        </div>
-        <div className="question">
-          <p>18</p>
-          <p>-</p>
-          <input type="number" />
-          <p>=</p>
-          <p>9</p>
-          <button>Check</button>
+        <div className="answer-section">
+          <button onClick={this.answerQuestions} id="answer-button">
+            Check Your Answers
+          </button>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default MathQuiz;
